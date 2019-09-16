a3e5928d15b416a6cfaca8070a479129395c35ee
Cole Giles <coleagiles@gmailc.com>
2019-09-16 08:57:07 -0500

    finalized chapter 3

--- src/assignments/CelsiusToFahrenheit.java
+++ src/assignments/CelsiusToFahrenheit.java
@@ -7,9 +7,21 @@ public class CelsiusToFahrenheit {
     public static void main(String[] args) {
 
        double temp;
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter a temperature>>>> ");
+       temp = input.nextDouble();
+
+       tempConversion(temp, "8:00");
+        tempConversion(temp, "12:00");
+        tempConversion(temp, "5:00");
+
 
     }
-    public static void eightTemp(double temp) {
+    public static void tempConversion(double temp, String time) {
+
+        double celsiusConversion = (temp - 32)*(5/9);
+        System.out.println("The temperature at " + time + " is " + celsiusConversion );
+
 
 
     }
